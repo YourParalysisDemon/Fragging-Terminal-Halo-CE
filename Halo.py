5e2650492f5ea77b1f3eb3cf6715c0ece6f365b9
YourParalysisDemon <145605048+YourParalysisDemon@users.noreply.github.com>
2024-10-11 17:22:57 -0400

    Update Halo.py
    Gave the Chief some hands.

--- Halo.py
+++ Halo.py
@@ -7,7 +7,7 @@ from threading import Thread
 from pymem import *
 from pymem.process import *
 from pymem.ptypes import RemotePointer
-# Password 
+
 while True:
     password = input("Enter password ")
     if password == "117":
@@ -15,19 +15,20 @@ while True:
         break
     else:
         print("Try again retard")
-# Game were hacking 
+
 mem = Pymem("MCC-Win64-Shipping")
-# DLL 
+
 module1 = module_from_name(mem.process_handle, "halo1.dll").lpBaseOfDll
-# Offsets 
+
 primary_offsets = [0X28A]
 fire_rate_offsets = [0X23A]
 shield_offsets = [0XA0]
 plasma_fire_rate_offsets = [0X204]
-grenade_offsets = [0X2FC]
-flashlight_offsets = [0X10, 0X10, 0X35C]
+plasma_ammo_offsets = [0X0]
 # This fucking sucked to find
 noclip_offsets = [0X4D8]
+melee1_offsets = [0X512]
+melee2_offsets = [0X513]
 
 
 def getpointeraddress(base, offsets):
@@ -38,7 +39,7 @@ def getpointeraddress(base, offsets):
         else:
             return remote_pointer.value + offset
 
-# Threads 
+
 def multi_run_117():
     new_thread = Thread(target=John117, daemon=True)
     new_thread.start()
@@ -59,18 +60,21 @@ def multi_run_plasma():
     new_thread.start()
 
 
-def John117():  
+def multi_run_hands():
+    new_thread = Thread(target=hands, daemon=True)
+    new_thread.start()
+
+
+def John117():
     addr1 = getpointeraddress(module1 + 0x01C38880, primary_offsets)
     addr2 = getpointeraddress(module1 + 0x01C38880, fire_rate_offsets)
     addr3 = getpointeraddress(module1 + 0x01C35AB0, shield_offsets)
-    addr4 = getpointeraddress(module1 + 0x01C35AB0, grenade_offsets)
 
     while 1:
         try:
             mem.write_int(addr1, 0x100)
             mem.write_int(addr2, 0xFFFFFFFF)
             mem.write_int(addr3, 0x47960000)
-            mem.write_int(addr4, 0x100)
         except pymem.exception.MemoryWriteError as e:
             print(f"Error writing memory: {e}")
         if keyboard.is_pressed("F1"):
@@ -103,19 +107,38 @@ def fuck_gravity():
             break
 
 
+def hands():
+    addr1 = getpointeraddress(module1 + 0x01C35AB0, melee1_offsets)
+    addr2 = getpointeraddress(module1 + 0x01C35AB0, melee2_offsets)
+
+    while 1:
+        try:
+            mem.write_int(addr1, 0x1)
+            mem.write_int(addr2, 0x1)
+        except pymem.exception.MemoryWriteError as e:
+            print(f"Error writing memory: {e}")
+        if keyboard.is_pressed("F1"):
+            mem.write_int(addr1, 0x0)
+            mem.write_int(addr2, 0x30)
+            break
+
+
 def plasma():
     addr1 = getpointeraddress(module1 + 0x01C38880, plasma_fire_rate_offsets)
+    addr2 = getpointeraddress(module1 + 0x01C38880, plasma_ammo_offsets)
 
     while 1:
         try:
             mem.write_int(addr1, 0x0)
+            mem.write_int(addr2, 0x100)
+
         except pymem.exception.MemoryWriteError as e:
             print(f"Error writing memory: {e}")
         if keyboard.is_pressed("F1"):
             mem.write_int(addr1, 0x0)
             break
 
-# GUI 
+
 pygame.init()
 pygame.mixer_music.load("music/mod.mp3")
 pygame.mixer_music.play(1)
@@ -126,7 +149,7 @@ root.wm_iconphoto(False, photo)
 root.attributes("-topmost", True)
 root.title("Fragging Terminal")
 root.configure(background='dark red')
-root.geometry("265x175")
+root.geometry("265x205")
 
 
 def callback(url):
@@ -141,16 +164,18 @@ def hide():
     root.withdraw()
 
 
-button1 = tk.Button(root, text="Haha gun go brrr", bg='black', fg='white', command=multi_run_117)
+button1 = tk.Button(root, text="Bullet go brrr", bg='black', fg='white', command=multi_run_117)
 button1.grid(row=1, column=0)
 button2 = tk.Button(root, text="No Clip", bg='black', fg='white', command=multi_run_clip)
 button2.grid(row=3, column=0)
-button3 = tk.Button(root, text="Plasma Firerate", bg='black', fg='white', command=multi_run_plasma)
+button3 = tk.Button(root, text="Plasma go brrr", bg='black', fg='white', command=multi_run_plasma)
 button3.grid(row=2, column=0)
-button4 = tk.Button(root, text="Fuck Gravity", bg='black', fg='white', command=multi_run_gravity)
-button4.grid(row=4, column=0)
+button3 = tk.Button(root, text="Fuck Gravity", bg='black', fg='white', command=multi_run_gravity)
+button3.grid(row=4, column=0)
+button4 = tk.Button(root, text="Throw Hands", bg='black', fg='white', command=multi_run_hands)
+button4.grid(row=5, column=0)
 button5 = tk.Button(root, text="Exit", bg='white', fg='black', command=root.destroy)
-button5.grid(row=5, column=0)
+button5.grid(row=6, column=0)
 label1 = tk.Label(master=root, text='- Show GUI', bg='red', fg='black')
 label1.grid(row=0, column=3)
 label2 = tk.Label(master=root, text='+ Hide GUI', bg='red', fg='black')
@@ -168,12 +193,13 @@ label5.grid(row=6, column=3)
 label6 = tk.Label(master=root, text='Main Loops', bg='red', fg='black')
 label6.grid(row=0, column=0)
 link1 = tk.Label(root, text="Your Sleep Paralysis Demon", bg="black", fg="red", cursor="hand2")
-link1.grid(row=6, column=0)
+link1.grid(row=7, column=0)
 link1.bind("<Button-1>", lambda e: callback("https://steamcommunity.com/profiles/76561198259829950/"))
 
 keyboard.add_hotkey("-", show)
 keyboard.add_hotkey("+", hide)
 keyboard.add_hotkey("5", multi_run_117)
+keyboard.add_hotkey("6", multi_run_plasma)
 keyboard.add_hotkey("V", multi_run_clip)
 keyboard.add_hotkey("G", multi_run_gravity)
 keyboard.add_hotkey("K", root.destroy)
